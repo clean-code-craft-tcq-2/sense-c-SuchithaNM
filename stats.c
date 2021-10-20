afc1322ddc619a9020d9e6b2b175bec1bd1030d4
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2021-10-20 20:50:30 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -9,3 +9,23 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+/* Adding function for Test case : raises alerts when max is greater than threshold */
+void emailAlerter(void)
+{
+    emailAlertCallCount++;
+}
+
+void ledAlerter(void)
+{
+    ledAlertCallCount++;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if(computedStats.max > maxThreshold )
+    {
+        alerters[0]();
+        alerters[1]();
+    }
+}
